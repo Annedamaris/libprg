f8cfbfc5ac1d05f852eac20d2c0308a7321fe385
Adriano Lima <adriano.lima@ifsc.edu.br>
2025-04-11 17:30:15 -0300

    Implementando a função criar_pilha

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,5 +1,7 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
+typedef struct pilha pilha_t;
+pilha_t* criar_pilha();
 
 #endif
--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "libprg/libprg.h"
 
 #define CAPACIDADE_INICIAL 10
@@ -7,3 +9,22 @@ typedef struct pilha {
     int topo;
     int capacidade;
 } pilha_t;
+
+pilha_t* criar_pilha() {
+
+    // Criar a estrutura pilha e alocar memória
+    pilha_t* pilha = (pilha_t*) malloc(sizeof(pilha_t));
+
+    // Alocar memória para os elementos da pilha
+    pilha->elementos = (int*) malloc(sizeof(int) * CAPACIDADE_INICIAL);
+
+    // Definir os valores iniciais da pilha
+    pilha->topo = -1;
+    pilha->capacidade = CAPACIDADE_INICIAL;
+
+    return pilha;
+}
+// push()
+// pop()
+// empty()
+// size()
